0271827372bbb8279120db14ff27dc5beaab126d
Nikolai Gavriushin <gna@live.ru>
2021-11-17 12:39:24 +0300

    feat: создать схемы и модели ресурсов API

---
+++ models/movies.js
@@ -0,0 +1,81 @@
+const mongoose = require('mongoose');
+const { isURL } = require('validator');
+
+const movieSchema = new mongoose.Schema({
+
+  // страна создания фильма
+  country: {
+    type: String,
+    required: true,
+  },
+  // режиссёр фильма
+  director: {
+    type: String,
+    required: true,
+  },
+  // длительность фильма
+  duration: {
+    type: Number,
+    required: true,
+  },
+  // год выпуска фильма
+  year: {
+    type: String,
+    required: true,
+  },
+  // описание фильма
+  description: {
+    type: String,
+    required: true,
+  },
+  // ссылка на постер к фильму
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => isURL(v),
+      message: 'Неправильный формат ссылки на постер к фильму',
+    },
+  },
+  // ссылка на трейлер фильма
+  trailer: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => isURL(v),
+      message: 'Неправильный формат ссылки на трейлер фильма',
+    },
+  },
+  // миниатюрное изображение постера к фильму
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => isURL(v),
+      message: 'Неправильный формат ссылки на миниатюрное изображение постера к фильму',
+    },
+  },
+  // _id пользователя, который сохранил фильм
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  // id фильма, который содержится в ответе сервиса MoviesExplorer
+  movieId: {
+    type: Number,
+    required: true,
+  },
+  // название фильма на русском языке
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  // название фильма на английском языке
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
